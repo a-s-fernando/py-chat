a45275c0ecc1b57ea4dac5fc41a329b5860b6132
a-s-fernando <fernandoalsth@gmail.com>
2023-06-22 11:27:19 +0100

    pulling out secret key to .env

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,4 +8,4 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 WTF_CSRF_ENABLED = True #this is used to prevent Cross-Site Request Forgery, increasing the security of our form input
-SECRET_KEY = '7ashf37haiiofja09d8'
+SECRET_KEY = os.environ.get('secret_key')
